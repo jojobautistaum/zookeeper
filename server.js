5926312a4209182aaa0f7d01d2f915fa12882fcd
Jojo Bautista <gjojob@yahoo.com>
2021-12-13 17:13:32 -0600

    req.query and req.param

--- server.js
+++ server.js
@@ -48,6 +48,16 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
+app.get('/api/animals/:id', (req,res) => {
+    const result = findById(req.params.id, animals);
+    res.json(result);
+});
+
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
